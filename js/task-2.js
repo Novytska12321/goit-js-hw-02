3d19bed05685fc8b3bbf6759314637f9a1ddcb9a
Novytska12321 <novytska@seznam.cz>
2024-06-26 21:50:02 +0200

    add

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,6 @@
 function formatMessage(message, maxLength) {
     
-    if (message.maxLength > maxLength) {
+    if (message.length > maxLength) {
         return message.slice(0, maxLength) + "...";
        
     } else {
